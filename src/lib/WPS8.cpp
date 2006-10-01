cc496902d6c44ed544348ee4cfd8bcedbcb0fa73
Andrew Ziehm <ahziem1@mailbolt.com>
2006-10-01 23:02:34 +0000

    -Works8: increase cfod again
    svn path=/libwps/; revision=25

--- src/lib/WPS8.cpp
+++ src/lib/WPS8.cpp
@@ -254,7 +254,7 @@ bool WPS8Parser::readFODPage(WPXInputStream * input, std::vector<FOD> * FODs, ui
 	uint16_t cfod = readU16(input); /* number of FODs on this page */
 
 	//fixme: what is the largest possible cfod?
-	if (cfod > 0x43)
+	if (cfod > 0x49)
 	{
 		WPD_DEBUG_MSG(("Works8: error: cfod = %i (0x%X)\n", cfod, cfod));
 		throw ParseException();
