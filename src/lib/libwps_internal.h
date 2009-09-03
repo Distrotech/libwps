f675771c4dab06de1ff8d9a93afc8af7092055a3
Fridrich Štrba <fridrich.strba@bluewin.ch>
2009-09-03 13:08:54 +0000

    Allow building for dos with djgpp toolchain
    svn path=/trunk/libwps/; revision=181

--- src/lib/libwps_internal.h
+++ src/lib/libwps_internal.h
@@ -28,16 +28,16 @@
 #include <libwpd/libwpd.h>
 #include <string>
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__DJGPP__)
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
-#else /* _MSC_VER */
+#else /* !_MSC_VER && !__DJGPP__*/
 #include <inttypes.h>
-#endif /* _MSC_VER */
+#endif /* _MSC_VER || __DJGPP__*/
 
 /* Various functions/defines that need not/should not be exported externally */
 
