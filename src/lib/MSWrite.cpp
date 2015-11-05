a7cdfab7e15c4ff15542d3e2db06b63e1cef9eee
Sean Young <sean@mess.org>
2015-11-05 11:45:30 +0000

    MsWrite: fcLim of the last pap can extend beyond end
    Fixes regression introduced in "c813ab4 Add Microsoft Word for DOS parser".

--- src/lib/MSWrite.cpp
+++ src/lib/MSWrite.cpp
@@ -809,6 +809,8 @@ shared_ptr<WPSContentListener> MSWriteParser::createListener(librevenge::RVNGTex
 
 	m_Main.setBegin(m_paragraphList[first].m_fcFirst);
 	m_Main.setEnd(m_paragraphList[i - 1].m_fcLim);
+	if (m_Main.end() > m_fcMac)
+		m_Main.setEnd(m_fcMac);
 	m_Main.setType("TEXT");
 
 	empty.setType("TEXT");
